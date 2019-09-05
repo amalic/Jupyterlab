0dfcf7d6f96b183819bd06d69e408e32f8ad44c1
Alexander Malic <alexander.malic@gmail.com>
2019-09-05 02:02:13 +0200

    Password needs to be hashed

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,7 @@ pip list -l
 CMD="jupyter lab --allow-root --ip=0.0.0.0 --no-browser"
 
 if [[ -v PASSWORD ]]; then
+  PASSWORD=$(python -c "import IPython; print(IPython.lib.security.passwd('$PASSWORD'))")
   CMD="$CMD --NotebookApp.token='' --NotebookApp.password='${PASSWORD}'"
 fi
 
